0e768b0053fef12a9aefe2d7d3040a85f3be1a7e
hamzabhiinder <hamzabhinder5@gmail.com>
2026-05-17 13:01:59 +0500

    Refactor AndroidManifest and MainActivity: update app label to use

--- android/app/src/main/kotlin/com/driverpassenger/app/driver_passenger_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/driverpassenger/app/driver_passenger_app/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.driverpassenger.app.driver_passenger_app
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+        // Avoid Android showing application label as a window title on some devices.
+        setTitle("")
+    }
+}
